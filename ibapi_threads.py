8c383062506f4e940302a636443153c78db1e63b
Petio Petrov <petioptrv@icloud.com>
2017-08-12 21:49:46 -0400

    Update ibapi_threads.py

--- ibapi_threads.py
+++ ibapi_threads.py
@@ -3,6 +3,7 @@ from ibapi.client import EClient
 from ibapi.contract import *
 
 import threading
+import time
 
 
 # Create your app class
@@ -10,17 +11,22 @@ class TestApp(EWrapper, EClient):
     def __init__(self):
         EWrapper.__init__(self)
         EClient.__init__(self, wrapper=self)
-        
+
         # CONNECTION OPTIONS
         self.socket_port = 7497  # Gateway: 4002; Workstation: 7496
         self.ip_address = "127.0.0.1"
         self.client_id = 0
 
+        # APP VARS
+        self.account = None  # Stores account number
+        self.reqID = None  # Used to store the next valid server request id
+
         self.connect(self.ip_address, self.socket_port, clientId=self.client_id)  # Connect to server
         print(f"serverVersion:{self.serverVersion()} connectionTime:{self.twsConnectionTime()}")  # Check connection
 
-    # Override wrapper methods (wrapper methods receive information from server; 
+    # OVERRIDEN WRAPPER METHODS (wrapper methods receive information from server;
     # check wrapper class for all available methods)
+
     def position(self, account: str, contract: Contract, position: float,
                  avgCost: float):
         """This event returns real-time positions for all accounts in
@@ -31,14 +37,40 @@ class TestApp(EWrapper, EClient):
               contract.secType, "Currency:", contract.currency,
               "Position:", position, "Avg cost:", avgCost)
 
+    def nextValidId(self, orderId: int):
+        """ Receives next valid order id. This is triggered once upon connection to server and
+            is received when the run function is called."""
+
+        self.reqID = orderId
+
+    def managedAccounts(self, accountsList: str):
+        """Receives a comma-separated string with the managed account ids. This is triggered
+           once upon connection to server and is received when the run function is called."""
+
+        self.account = accountsList.split(',')[0]  # Exctract the acc number from the comma-delimited list
+
+    # OTHER METHODS
+
     # This is the loop which will be running parallel to the scanner receiving information from the server.
     # This is where the logic of your app goes
     def loop(self):
-        usr_in = input('\nWhat to do next?')
-        while usr_in != 'exit':
-            usr_in = input('What to do next?')
+
+        # This makes sure we receive the account number and next valid request id
+        # both of which are not used in this script, but might be useful for more advanced things
+        # like passing orders etc.
+        time.sleep(2)
+
+        # USER MENU
+
+        while True:
+            time.sleep(2) # To allow server messages to be printed out
+            usr_in = input('\nWhat to do next? (p for open positions):')
             if usr_in == 'p':
                 self.reqPositions()
+
+    # Used to increment the id used to make requests to the server
+    def increment_id(self):
+        self.reqID += 1
 
 
 def main():
@@ -50,6 +82,7 @@ def main():
     # Start both threads
     reader_thread.start()
     loop_thread.start()
+
 
 if __name__ == '__main__':
     main()
